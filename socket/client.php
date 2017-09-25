7476718b87d53f9f932c0d75fb515608bd2b076f
William <willrpg@hotmail.com>
2017-09-24 22:35:57 -0300

    fim primeira parte

--- socket/client.php
+++ socket/client.php
@@ -1,29 +1,17 @@
 <?php
 error_reporting(E_ALL);
-
-echo "<h2>TCP/IP Connection</h2>\n";
-
-/* Get the port for the WWW service. */
-//$service_port = getservbyname('www', 'tcp');
 $service_port = 8081;
-
-/* Get the IP address for the target host. */
 $address = gethostbyname('127.0.0.1');
 
 /* Create a TCP/IP socket. */
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 if ($socket === false) {
     echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n";
-} else {
-    echo "OK.\n";
 }
 
-echo "Attempting to connect to '$address' on port '$service_port'...";
 $result = socket_connect($socket, $address, $service_port);
 if ($result === false) {
     echo "socket_connect() failed.\nReason: ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
-} else {
-    echo "OK.\n";
 }
 
 $in = "HEAD / HTTP/1.1\r\n";
@@ -31,14 +19,14 @@ $in .= "Host: {$address}\r\n";
 $in .= "Connection: Close\r\n\r\n";
 $out = '';
 
-echo "Sending HTTP HEAD request...";
 socket_write($socket, $in, strlen($in));
-echo "OK.\n";
 
-echo "Reading response:\n\n";
-while ($out = socket_read($socket, 2048)) {
-    echo $out;
-}
+// while ($out = socket_read($socket, 2048)) {
+//    echo $out;
+// }
+
+$out = socket_read($socket, 2048);
+echo $out;
 
 echo "Closing socket...";
 socket_close($socket);
